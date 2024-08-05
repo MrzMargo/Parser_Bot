333e9335f7a6d881929f93c5ebc99fcae09f825a
Margo <mrzmargo08@gmail.com>
2024-08-05 20:46:01 +0300

    changing bot-hepler

--- pars.py
+++ pars.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
-
-# URL страницы для парсинга
+import pprint
+"""# URL страницы для парсинга
 url = "https://ram.by/computers.html"
 
 # Отправляем GET-запрос и получаем HTML-код страницы
@@ -15,7 +15,10 @@ soup = BeautifulSoup(html_content, "html.parser")
 article_titles = soup.find_all("div", class_="items list-view")
 #print(article_titles)
 pretty_html = soup.prettify()
-print(pretty_html)
-# Выводим заголовки статей
-"""for title in article_titles:
-    print(title.text.strip())"""
+print(pretty_html)"""
+
+"""params = {'q': 'python'}
+response = requests.get('https://api.github.com/search/repositories', params=params)
+
+response_json = response.json()
+print(response_json['total_count'])"""
