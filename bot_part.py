69d648e07872c03afc21b6e88a96e86dd7b97298
Margo <mrzmargo08@gmail.com>
2024-08-16 23:47:45 +0300

    data-base added

--- bot_part.py
+++ bot_part.py
@@ -1,61 +1,45 @@
 import telebot
 from telebot import types
+import sqlite3
+import datetime
+import time
 # Замените TOKEN на ваш токен бота
 TOKEN = '7410644155:AAHdQ19Wd8rkZ8dy8dNZ1yEhZCtvFrB5sGk'
 
-"""# Создаем экземпляр бота
-bot = telebot.TeleBot(TOKEN)
+conn = sqlite3.connect('tasks.db')
+c = conn.cursor()
 
-# Команда "/start"
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    markup = types.ReplyKeyboardMarkup(row_width=2)
-    item1 = types.KeyboardButton("Кнопка 1")
-    item2 = types.KeyboardButton("Кнопка 2")
-    markup.add(item1, item2)
+c.execute('''CREATE TABLE IF NOT EXISTS Status
+             (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+              name_of_status TEXT NOT NULL)''')
+#c.execute('INSERT INTO Status (name_of_status) VALUES (?)', ("Сделать",))
+#c.execute('INSERT INTO Status (name_of_status) VALUES (?)', ("В процессе",))
+#c.execute('INSERT INTO Status (name_of_status) VALUES (?)', ("Готово",))
 
-    bot.send_message(chat_id=message.chat.id, text="Привет! Выберите одну из кнопок:", reply_markup=markup)
 
-# Обработчик для кнопок
-@bot.message_handler(content_types=['text'])
-def button_handler(message):
-    if message.text == "Кнопка 1":
-        bot.send_message(chat_id=message.chat.id, text="Вы нажали Кнопку 1")
-    elif message.text == "Кнопка 2":
-        bot.send_message(chat_id=message.chat.id, text="Вы нажали Кнопку 2")
-    else:
-        bot.send_message(chat_id=message.chat.id, text="Привет, мир!")
 
-# Запуск бота
-bot.polling(none_stop=True)"""
-"""# Создаем экземпляр бота
-bot = telebot.TeleBot(TOKEN)
+c.execute('''CREATE TABLE IF NOT EXISTS Notification
+             (id INTEGER PRIMARY KEY AUTOINCREMENT,
+              time TEXT NOT NULL)''')
+#c.execute('INSERT INTO Notification (time) VALUES (?)', ("None",))
 
-# Команда "/start"
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    # Создаем клавиатуру с кнопками
-    keyboard = types.InlineKeyboardMarkup()
-    button1 = types.InlineKeyboardButton(text="Кнопка 1", callback_data="button1")
-    button2 = types.InlineKeyboardButton(text="Кнопка 2", callback_data="button2")
-    keyboard.add(button1, button2)
 
-    # Отправляем сообщение с клавиатурой
-    bot.send_message(chat_id=message.chat.id, text="Привет! Выберите одну из кнопок:", reply_markup=keyboard)
+c.execute('''CREATE TABLE IF NOT EXISTS Task
+             (id INTEGER PRIMARY KEY AUTOINCREMENT,
+              name_of_task TEXT NOT NULL, 
+              status_id INTEGER NOT NULL,
+              notification_id INTEGER NOT NULL,
+              FOREIGN KEY (status_id) REFERENCES Status(id),
+              FOREIGN KEY (notification_id) REFERENCES Notification(id))''')
 
-# Обработчик для нажатия кнопок
-@bot.callback_query_handler(func=lambda call: True)
-def handle_button_click(call):
-    if call.data == "button1":
-        bot.send_message(chat_id=call.message.chat.id, text="Вы нажали Кнопку 1")
-    elif call.data == "button2":
-        bot.send_message(chat_id=call.message.chat.id, text="Вы нажали Кнопку 2")
-
-# Запуск бота
-bot.polling(none_stop=True)"""
+conn.commit()
+c.close()
 
 bot = telebot.TeleBot(TOKEN)
 user_states = {}
+
+
+
 # Команда "/start"
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -78,28 +62,61 @@ def send_welcome(message):
 
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
-        user_id = message.chat.id
-        if user_id in user_states and user_states[user_id] == 'waiting_for_task':
-            task_text = message.text
-            print(f"Новое дело: {task_text}")
-            with open("user_messages.txt", "a") as file:
-                for user_id, message in data.items():
-                    file.write(f"User ID: {user_id}, Message: {message}\n")
+        conn = sqlite3.connect('tasks.db')
+        c = conn.cursor()
 
+        user_id = message.chat.id
+        def reminders(user_id):
+            date_time_str = "2024-08-16 23:35"
+            date_time = datetime.datetime.strptime(date_time_str, '%Y-%m-%d %H:%M')
+
+            while True:
+                now = datetime.datetime.now()
+                if now >= date_time:
+                    bot.send_message(chat_id=user_id, text="Напоминание: Пора сделать что-то важное!")
+                    break
+                time.sleep(1)
+        global name_of_task
+
+
+        if user_id in user_states and user_states[user_id] == 'waiting_for_task':
+
+            name_of_task = message.text
+            status_id = 1
+            notification_id = 1
+
+            c.execute("INSERT INTO Task (name_of_task, status_id, notification_id) VALUES (?, ?, ?)",
+                      (name_of_task, status_id, notification_id))
             del user_states[user_id]
             bot.send_message(chat_id=user_id, text="Дело добавлено. Хотите установить напоминание?")
+
             user_states[user_id] = 'adding_timer'
         elif user_id in user_states and user_states[user_id] == 'adding_timer':
             if message.text == "да":
-                print("Добавляем таймер")
-                bot.send_message(chat_id=user_id, text="Напоминание установлено!")
+                bot.send_message(chat_id=user_id, text="В какое время вам напомнить об этой задаче?")
                 del user_states[user_id]
+                user_states[user_id] = 'waiting_for_time'
             elif message.text == "нет":
                 print("Не добавляем")
+
                 bot.send_message(chat_id=user_id, text="Хорошо, эта задача будет без напоминания")
                 del user_states[user_id]
             else:
                 bot.send_message(chat_id=user_id, text="На это у меня нет ответа...")
+        elif user_id in user_states and user_states[user_id] == 'waiting_for_time':
+            notification_time = message.text #"2023-08-16 10:30:00"
+            del user_states[user_id]
+            c.execute("INSERT INTO Notification (time) VALUES (?)", (notification_time,))
+            bot.send_message(chat_id=user_id, text="Напоминание добавлено!")
+            c.execute("SELECT id FROM Notification WHERE time = ?", (notification_time,))
+            notification_id = c.fetchone()[0]
+            c.execute("UPDATE Task SET notification_id = ? WHERE name_of_task = ?",(notification_id, name_of_task))
+            print(notification_id)
+            print(notification_time)
+            reminders(user_id)
+
+        conn.commit()
+        conn.close()
 
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -110,5 +127,6 @@ def handle_button_click(call):
         bot.send_message(chat_id=call.message.chat.id, text="Вы нажали Кнопку 2")
     elif call.data == "button3":
         bot.send_message(chat_id=call.message.chat.id, text="Вы нажали Кнопку 3")
+
 
 bot.polling(none_stop=True)
